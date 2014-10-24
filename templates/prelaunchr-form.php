23387c2d8fe4a0000c6bf7b25f52776beb144c68
Jason Conroy <jason@findingsimple.com>
2014-10-24 12:07:14 +1100

    Display error if invalid email address

--- templates/prelaunchr-form.php
+++ templates/prelaunchr-form.php
@@ -7,6 +7,7 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
 }
 ?>
 <div class="prelaunchr">
+	<div class="response"></div>
 	<form class="pform" action="">
 		<label for="email">Email Address: <span class="required">*</span></label>  
 		<input type="email" id="email" name="email" value="" placeholder="your@email.com" required="required" />  
