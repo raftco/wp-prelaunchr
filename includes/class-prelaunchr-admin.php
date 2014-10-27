18a220e7070094756a91c1228f87ed6463d48118
Jason Conroy <jason@findingsimple.com>
2014-10-27 14:27:23 +1100

    Clean up CSV export button functionality
    Include the botton below the list table

--- includes/class-prelaunchr-admin.php
+++ includes/class-prelaunchr-admin.php
@@ -17,23 +17,11 @@ class Prelaunchr_Admin {
 			array( $this, 'render_list_page' )
 		);
 
-		add_submenu_page(
-			'prelaunchr', 
-        	'Download CSV',
-        	'Download CSV',
-        	'download_csv',
-        	'download'
-        );
-
-		add_action('admin_init',array($this,'download_csv'));
+		add_action( 'admin_init' , array( $this , 'download_csv' ) );
 
 	} 
 
 	public function render_list_page(){
-
-		if ( $_GET['download'] ) {
-			var_dump('poo');
-		}
 
 		/**
 		 * Create an instance or our table
@@ -50,20 +38,22 @@ class Prelaunchr_Admin {
 
 			<h2>Prelaunchr</h2>
 			
-			<div style="background:#ECECEC;border:1px solid #CCC;padding:0 10px;margin-top:5px;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px;">
-
-				<p>Download emails as: <a href="?download=csv" target="_blank" style="text-decoration:none;">CSV</a></p>
-
-			</div>
-			
-			<!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
-			<form id="movies-filter" method="get">
-				<!-- For plugins, we also need to ensure that the form posts back to our current page -->
+			<form id="prelaunchr-filter" method="get">
 				<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
-				<!-- Now we can render the completed list table -->
 				<?php $Prelaunchr_List_Table->display() ?>
 			</form>
-			
+
+			<h2>CSV Export</h2>
+
+			<form method="post" action="">
+				<p>The Download button below generates a CSV file containing the email addresses, number of referrals, time of signup etc. which an be used to import into a mailing system of your choice.</p>
+				<p class="submit">
+					<input type="submit" name="prelaunchr_download_csv" id="prelaunchr_download_csv" class="button" value="Download CSV File &raquo;"  />
+				</p>
+				<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+				<?php wp_nonce_field(); ?>
+			</form>
+
 		</div>
 		<?php
 	}
@@ -71,43 +61,36 @@ class Prelaunchr_Admin {
 
 	public function download_csv(){
 
+		if ( is_admin() && ! empty( $_POST['prelaunchr_download_csv'] ) && check_admin_referer() && current_user_can('manage_options') ) {
+
 			/**
 			 * parsecsv-for-php library
 			 */
 			require_once PRELAUNCHR_PLUGIN_PATH . '/lib/parsecsv-for-php/parsecsv.lib.php';
 
+			global $wpdb;
 
-global $pagenow;
+			/**
+			 * Our table name
+			 */
+			$table_name = $wpdb->prefix . "prelaunchr";
 
-      if ($pagenow=='admin.php' &&  
-          isset($_GET['page'])  && 
-          $_GET['page']=='download') {
+			/**
+			 * Prepare the query
+			 */
+			$query = "SELECT id,email,pid,rmail as referrer, count as referrals, time FROM $table_name A
+						LEFT JOIN ( SELECT rid ,COUNT(*) as count FROM $table_name GROUP BY rid ORDER BY count DESC ) B 
+						ON A.id = B.rid
+						LEFT JOIN ( SELECT id as rid,email as rmail FROM $table_name ) C 
+						ON A.rid = C.rid";
 
+			$results = $wpdb->get_results($query, ARRAY_A);
 
-		global $wpdb;
+			$csv = new parseCSV();
 
-		/**
-		 * Our table name
-		 */
-		$table_name = $wpdb->prefix . "prelaunchr";
+			$csv->output('data.csv', $results, array('id', 'email', 'pid', 'referrer', 'referrals', 'time'), ',');
 
-   		/**
-   		 * Prepare the query
-   		 */
-		//$query = "SELECT id,email,pid,rid,COALESCE(count, 0) as referrals FROM $table_name A LEFT JOIN ( SELECT rid as countid,COUNT(*) as count FROM $table_name GROUP BY rid ORDER BY count DESC ) B ON A.id = B.countid";
-   		$query = "SELECT id,email,pid,rmail as referrer, count as referrals, time FROM $table_name A
-LEFT JOIN ( SELECT rid ,COUNT(*) as count FROM $table_name GROUP BY rid ORDER BY count DESC ) B 
-ON A.id = B.rid
-LEFT JOIN ( SELECT id as rid,email as rmail FROM $table_name ) C 
-ON A.rid = C.rid";
-
-		$results = $wpdb->get_results($query, ARRAY_A);
-
-		$csv = new parseCSV();
-
-		$csv->output('data.csv', $results, array('id', 'email', 'pid', 'referrer', 'referrals', 'time'), ',');
-
-				exit(); // all done
+			exit(); // all done
 
 		}
 
