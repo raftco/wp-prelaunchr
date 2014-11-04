655564935afbb76e3e1e2319d4e0ac1af60820f9
Jason Conroy <jason@findingsimple.com>
2014-11-04 16:46:38 +1100

    Clean up prelaunchr data on uninstall
    * Drop the prelaunchr table + entries
    * Delete the reward cpt posts and post meta
    
    Closes #7

--- uninstall.php
+++ uninstall.php
@@ -8,3 +8,16 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+global $wpdb;
+
+/**
+ * Delete prelaunchr table with entries
+ */
+$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . "prelaunchr" );
+
+/**
+ * Delete rewards CPT posts and post meta
+ */
+$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'reward' );" );
+$wpdb->query( "DELETE FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE wp.ID IS NULL;" );
