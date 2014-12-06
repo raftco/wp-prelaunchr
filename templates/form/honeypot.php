aa15996a3768cf0bbe9e69090e6342e34da6e806
Jason Conroy <jason@findingsimple.com>
2014-12-07 09:57:17 +1100

    Avoid FOUC with honeypot field

--- templates/form/honeypot.php
+++ templates/form/honeypot.php
@@ -6,5 +6,5 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
 	die ( 'You do not have sufficient permissions to access this page!' );
 }
 ?>
-<label for="name" class="ignore"><?php _e('Name:', 'prelaunchr'); ?></label>
-<input type="text" class="ignore" id="name" name="name" value="" />
+<label for="name" class="ignore" style="display:none;"><?php _e('Name:', 'prelaunchr'); ?></label>
+<input type="text" class="ignore" id="name" name="name" value="" style="display:none;"/>
